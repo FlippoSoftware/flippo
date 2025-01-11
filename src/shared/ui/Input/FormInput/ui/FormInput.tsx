d0f6b182938fede3dbb736f02fad81c7363ebdb7
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2025-01-11 22:17:29 +0300

    Refactor: FormInput wrapped to  forwardRef

--- src/shared/ui/Input/FormInput/ui/FormInput.tsx
+++ src/shared/ui/Input/FormInput/ui/FormInput.tsx
@@ -1,16 +1,17 @@
 import { Input } from '@shared/ui/Input';
 import { clsx } from 'clsx';
+import { type ForwardedRef, forwardRef } from 'react';
 
 import type { TFormInputProps } from '../types/TFormInputProps';
 
 import st from './FormInput.module.scss';
 
-function FormInput(props: TFormInputProps) {
+function FormInput(props: TFormInputProps, ref: ForwardedRef<HTMLInputElement>) {
   const { className, errorMessage, ...otherProps } = props;
 
   return (
     <div className={clsx(st.inputGroup, !!errorMessage && st.show)}>
-      <Input className={clsx(!!errorMessage && st.invalid, className)} {...otherProps} />
+      <Input className={clsx(!!errorMessage && st.invalid, className)} ref={ref} {...otherProps} />
       <div className={clsx(st.errorWrap, !!errorMessage && st.show)}>
         <p className={st.error}>{errorMessage}</p>
       </div>
@@ -18,4 +19,4 @@ function FormInput(props: TFormInputProps) {
   );
 }
 
-export default FormInput;
+export default forwardRef(FormInput);
