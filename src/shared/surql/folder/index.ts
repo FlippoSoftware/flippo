cfaa9515d1e65bc400d6b4727a1f99034cfa5082
BlackPoretsky <84518407+BlackPoretsky@users.noreply.github.com>
2025-01-15 14:32:53 +0300

    Feat: add SurrealQL queries for managing sets, tags, cards, folders, publications, and repetitions

---
+++ src/shared/surql/folder/index.ts
@@ -0,0 +1,5 @@
+import createFolder from './createFolder.surql?raw';
+import deleteFolder from './deleteFolder.surql?raw';
+import folder from './folder.surql?raw';
+
+export { createFolder, deleteFolder, folder };
---
+++ src/shared/surql/repetition/index.ts
@@ -0,0 +1,6 @@
+import createRepetition from './createRepetition.surql?raw';
+import deleteRepetition from './deleteRepetition.surql?raw';
+import repetition from './repetition.surql?raw';
+import updateRepetition from './updateRepetition.surql?raw';
+
+export { createRepetition, deleteRepetition, repetition, updateRepetition };
